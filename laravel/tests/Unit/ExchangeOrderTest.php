fdbe77c952276a910f04f915bbdb89b2759b5a0a
sara <mohamadisa996@gmail.com>
2024-04-10 17:28:13 +0330

    init

--- laravel/tests/Unit/ExchangeOrderTest.php
+++ laravel/tests/Unit/ExchangeOrderTest.php
@@ -30,34 +30,34 @@ class ExchangeOrderTest extends TestCase
         $response->assertStatus(201)
             ->assertJson(['tracking_code' => 'RANDOM1234']);
     }
-//    public function test_can_show_exchange_order()
-//    {
-//        $trackingCode = 'RANDOM1234';
-//
-//        $mockRepository = $this->createMock(ExchangeOrderRepositoryInterface::class);
-//        $mockRepository->expects($this->once())
-//            ->method('findByTrackingCode')
-//            ->with($this->equalTo($trackingCode))
-//            ->willReturn((object)[
-//                'email' => 'user@example.com',
-//                'source_currency' => 'layer',
-//                'target_currency' => 'bitcoin',
-//                'amount' => 1000.00,
-//                'tracking_code' => $trackingCode,
-//            ]);
-//
-//        $this->app->instance(ExchangeOrderRepositoryInterface::class, $mockRepository);
-//
-//        $response = $this->getJson("/api/exchange-orders/{$trackingCode}");
-//
-//        $response->assertStatus(200)
-//            ->assertJson([
-//                'email' => 'user@example.com',
-//                'source_currency' => 'layer',
-//                'target_currency' => 'bitcoin',
-//                'amount' => 1000.00,
-//                'tracking_code' => $trackingCode,
-//            ]);
-//    }
+    public function test_can_show_exchange_order()
+    {
+        $trackingCode = 'RANDOM1234';
+
+        $mockRepository = $this->createMock(ExchangeOrderRepositoryInterface::class);
+        $mockRepository->expects($this->once())
+            ->method('findByTrackingCode')
+            ->with($this->equalTo($trackingCode))
+            ->willReturn((object)[
+                'email' => 'user@example.com',
+                'source_currency' => 'layer',
+                'target_currency' => 'bitcoin',
+                'amount' => 1000.00,
+                'tracking_code' => $trackingCode,
+            ]);
+
+        $this->app->instance(ExchangeOrderRepositoryInterface::class, $mockRepository);
+
+        $response = $this->getJson("/api/exchange-orders/{$trackingCode}");
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'email' => 'user@example.com',
+                'source_currency' => 'layer',
+                'target_currency' => 'bitcoin',
+                'amount' => 1000.00,
+                'tracking_code' => $trackingCode,
+            ]);
+    }
 
 }
